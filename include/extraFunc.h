d2b6a1fc489b7d0e50de30b40fab5f7d77eb2a04
Marcus Lind <marcus.lind@afconsult.com>
2017-09-06 10:53:32 +0200

    wip on new feature that will return a big rect

--- include/extraFunc.h
+++ include/extraFunc.h
@@ -2,3 +2,5 @@
 #include <opencv2/opencv.hpp>
 
 void printCenterCoord(cv::Point coord);
+
+cv::Rect largerRect(cv::Rect rect1, cv::Rect2d rect2);
--- src/extraFunc.cpp
+++ src/extraFunc.cpp
@@ -3,3 +3,24 @@
 void printCenterCoord(cv::Point coord) {
     std::cout << coord << std::endl;
 }
+
+cv::Rect largerRect(cv::Rect rect1, cv::Rect2d rect2) {
+    cv::Rect newRect;
+
+    if (rect1.x < rect2.x) {
+        newRect.x = rect1.x;
+    }
+    else {
+        newRect.x = rect2.x;
+    }
+
+    if (rect1.y < rect2.y) {
+        newRect.y = rect1.y;
+    }
+    else {
+        newRect.y = rect2.y;
+    }
+    //not done yet, trying to make a large rect that covers both eyes
+
+    return newRect;
+}
